b9dbb7ad5ed1d3a166c4ac91afbf0ffe1f6d5ba8
joe429273392 <a429273392@qq.com>
2018-05-30 08:36:35 +0800

    first

--- src/App.js
+++ src/App.js
@@ -3,47 +3,69 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-constructor(props){
-    super(props);
-    //保存我们输入的这些记录
-    this.state = {
-        lists:['真的很ok','我觉得ok','ojbk'],
-        nowContent:'',
-    };
+  constructor(props){
+      super(props);
+      //保存我们输入的这些记录
+      this.state = {
+          username : '',
+          password : '',
+      };
+      
+
+  }
+
+  getContent = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handleLoginClick = async () => {
+    // const that = this;
+
+    // const body = { ...that.state };
+    // const rs = await fetch('http://localhost:4000/login', {
+    //   header: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(body),
+    //   "method": "POST",
+    // })
+
+    // const res = await rs.json();
+    // console.log(res);
+
     
-    this.getInputContent = this.getInputContent.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-}
 
-getInputContent(event){
-  this.setState({nowContent:event.target.value})
-}
-
-handleClick(event){
-  let lists = this.state.lists;
-
-  lists.push(this.state.nowContent);
-  this.setState({
-    lists: lists,
-  })
-}
+    fetch('http://localhost:4000/login', {
+      body: JSON.stringify({
+        "userName": "tom",
+        "name": "tom",
+        "identity": "teacher",
+        "isSelected": false
+      }),
+      header: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+    }).then(function(req) {
+      return req.json();
+    }).then(function(rs) {
+      console.log(rs);
+    })
+  }
 
   render() {
     console.log(this.state.nowContent);
     return (
+      <div className="App">
         <div className="App">
-            <input placeholder="输入..." type="text" value={this.state.nowContent} 
-              onChange={this.getInputContent} />
-            <button className="btn" onClick={this.handleClick} type="button">确认</button>
-
-            <div className="displayBox">
-            {
-              this.state.lists.map(item => (
-                <div className="items"><p key={item}>{item}</p></div>
-              ))
-            }
-            </div>
+            <input type="text" name="username" value={this.state.username} onChange={this.getContent} />
+            <input type="password" name="password" value={this.state.password} onChange={this.getContent} />
+            <button type="button" onClick={this.handleLoginClick} >登录</button>
         </div>
+      </div>
+        
 
     );
   }
